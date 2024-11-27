412bc6dc163783b60720d808f973ab6c1502acb9
Chandler Berry <chandler.r.berry@gmail.com>
2024-11-26 21:02:08 -0500

    add languages to treesitter

--- lua/chndlr/plugins/treesitter.lua
+++ lua/chndlr/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     	config = function () 
 	local configs = require("nvim-treesitter.configs")
 		configs.setup({
-		ensure_installed = {"c", "python", "vim", "rust", "yaml"},
+		ensure_installed = {"c", "go", "cpp", "zig", "bash", "python", "vim", "rust", "yaml", "dockerfile", "tmux", "sway", "sql", "nginx"},
 		sync_install = false,
 		highlight = { enable = true },
 		indent = { enable = true },
