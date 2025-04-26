c6118833afeeea48d890609a69284bec75db5a48
Chandler Berry <chandler.r.berry@proton.me>
2025-04-26 12:19:55 -0400

    fix treesitter.lua formatting

--- lua/chndlr/plugins/treesitter.lua
+++ lua/chndlr/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 -- configure treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-	  local config = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
 		config.setup({
-      ensure_installed = {
-        "c",
-        "go",
-        "cpp",
-        "bash",
-        "python",
-        "vim",
-        "rust",
-        "yaml",
-        "dockerfile",
-        "tmux",
-        "sql",
-        "nginx",
-        "lua",
-        "toml",
-        "json",
-        "jq",
-        "html",
-        "terraform",
-        "caddy",
-      },
-      auto_install=true,
-		  sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-	  })
-  end
+			ensure_installed = {
+				"c",
+				"go",
+				"cpp",
+				"bash",
+				"python",
+				"vim",
+				"rust",
+				"yaml",
+				"dockerfile",
+				"tmux",
+				"sql",
+				"nginx",
+				"lua",
+				"toml",
+				"json",
+				"jq",
+				"html",
+				"terraform",
+				"caddy",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
