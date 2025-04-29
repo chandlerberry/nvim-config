dc4c888df723387e53bfe8afbda990920c348eb6
Chandler Berry <chandler.r.berry@proton.me>
2025-04-28 22:26:37 -0400

    tabs, colorscheme, and trouble

--- lua/chndlr/plugins/colorscheme.lua
+++ lua/chndlr/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
 	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-	    vim.cmd([[colorscheme tokyonight]])
+		vim.cmd([[colorscheme tokyonight]])
 	end,
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
 }
