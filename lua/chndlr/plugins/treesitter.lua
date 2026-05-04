aabcff92d92e404ad666268640c986b850b007e6
Chandler Berry <chandler.r.berry@proton.me>
2026-05-04 14:26:38 -0400

    update treesitter config

--- lua/chndlr/plugins/treesitter.lua
+++ lua/chndlr/plugins/treesitter.lua
@@ -1,42 +1,43 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local ts = require("nvim-treesitter")
 
-		ts.install({
-			"c",
-			"go",
-			"cpp",
-			"bash",
-			"python",
-			"vim",
-			"rust",
-			"yaml",
-			"dockerfile",
-			"tmux",
-			"sql",
-			"nginx",
-			"lua",
-			"toml",
-			"json",
-			"jq",
-			"html",
-			"terraform",
-			"caddy",
-		})
+    ts.install({
+      "c",
+      "go",
+      "cpp",
+      "bash",
+      "python",
+      "vim",
+      "rust",
+      "yaml",
+      "dockerfile",
+      "tmux",
+      "sql",
+      "nginx",
+      "lua",
+      "toml",
+      "json",
+      "jq",
+      "html",
+      "terraform",
+      "caddy",
+      "typescript",
+    })
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "*",
-			callback = function()
-				-- auto-install parser for unrecognized filetypes (replaces auto_install)
-				pcall(ts.install, { vim.bo.filetype })
-				-- enable highlighting and indentation if a parser is available
-				if pcall(vim.treesitter.start) then
-					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        -- auto-install parser for unrecognized filetypes (replaces auto_install)
+        pcall(ts.install, { vim.bo.filetype })
+        -- enable highlighting and indentation if a parser is available
+        if pcall(vim.treesitter.start) then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
 }
